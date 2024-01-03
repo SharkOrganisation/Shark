be28596a8893f124631f40830a069cc8bc9dc794
waelnasseur1 <waelnasseur6@gmail.com>
2024-01-03 16:33:56 +0100

    pull to push

--- client/App.js
+++ client/App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import WelcomeScreen from './screens/welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>hello world</Text>
       <StatusBar style="auto" />
-      <WelcomeScreen/>
     </View>
   );
 }
